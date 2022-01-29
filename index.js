30719b6c725201dd4a7ed41c41b28e880987c588
Ali <29aliraza1998@gmail.com>
2022-01-29 20:08:47 +0500

    change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //Node server which will handle socket io connections
-const io=require('socket.io')(process.env.PORT || 80,{
+var io=require('socket.io')(process.env.PORT || 3000 ,{
     cors:{
         origin:"*"
     }
--- js/client.js
+++ js/client.js
@@ -1,4 +1,4 @@
-const socket=io('http://localhost:80');
+const socket=io('http://localhost:3000');
 //Get DOM elements in respective js variables
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
